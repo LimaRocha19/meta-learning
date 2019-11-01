1f5d9281992189dd984a99e2def85dfc76d31331
Isaías Lima <isaiahlima18@gmail.com>
2019-11-01 09:14:20 -0300

    Implemented! Let's finish this article

--- meta-regressor.py
+++ meta-regressor.py
@@ -23,7 +23,7 @@ def outliers(df):
 
     return outliers
 
-def knn(df, target):
+def knn(df, target, k=3):
 
     if 'Unnamed: 0' in df.columns:
         del df['Unnamed: 0'] # a small workaround regarding csv exporting issues
@@ -118,19 +118,26 @@ def knn(df, target):
 
     distances = pd.DataFrame({
         'db': db,
-        'distances': d
+        'distances': d,
+        'cart_accuracy': y['cart_accuracy'],
+        'naive_accuracy': y['naive_accuracy'],
+        'neural_accuracy': y['neural_accuracy']
     })
 
     distances = distances.sort_values(by=['distances'])
-    near_db = distances.iloc[0,0]
 
-    # print(distances)
+    cart_acc = []
+    naive_acc = []
+    neural_acc = []
 
-    ranking = y[y['db']==near_db]
+    for i in range(k): # n neighbors
+        cart_acc.append(distances['cart_accuracy'].iloc[i])
+        naive_acc.append(distances['naive_accuracy'].iloc[i])
+        neural_acc.append(distances['neural_accuracy'].iloc[i])
 
-    cart = ranking['cart_accuracy'].iloc[0]
-    naive = ranking['naive_accuracy'].iloc[0]
-    neural = ranking['neural_accuracy'].iloc[0]
+    cart = np.mean(cart_acc)
+    naive = np.mean(naive_acc)
+    neural = np.mean(neural_acc)
 
     recomendation = ''
 
@@ -139,112 +146,6 @@ def knn(df, target):
     if naive > cart and naive > neural:
         recomendation = 'naive'
     if neural > cart and neural > naive:
-        recomendation = 'neural'
-
-    return recomendation + '(' + near_db + ')'
-
-def linear(df, target):
-
-    if 'Unnamed: 0' in df.columns:
-        del df['Unnamed: 0'] # a small workaround regarding csv exporting issues
-
-    # metadata extraction for the new database
-
-    df_examples = np.log2(df.count()[0])
-    df_attributes = np.log2(len(df.columns))
-    df_discrete_ratio = len(df.select_dtypes(include=['int64']).columns) / len(df.columns)
-
-    df_classes = len(df[target].value_counts())
-    df_entropy = stats.entropy(df[target], base=2)
-
-    df_outliers = outliers(df) / df.count()[0]
-
-    entropies = []
-
-    for column in df.select_dtypes(include=['int64']).columns:
-        if not(np.isnan(stats.entropy(df[column], base=2))):
-            entropies.append(stats.entropy(df[column], base=2))
-
-    df_mean_entropy = np.mean(entropies)
-
-    for column in df.select_dtypes(include=['int64']).columns:
-        del df[column]
-
-    df_mean_correlation = np.abs(df.corr()).mean().mean()
-
-    df_mean_skew = df.skew().mean()
-
-    df_mean_kurtosis = df.kurtosis().mean()
-
-    table = {
-        'examples': df_examples,
-        'attributes': df_attributes,
-        'discrete_ratio': df_discrete_ratio,
-        'mean_entropy': df_mean_entropy,
-        'mean_correlation': df_mean_correlation,
-        'mean_skew': df_mean_skew,
-        'mean_kurtosis': df_mean_kurtosis,
-        'outliers': df_outliers,
-        'classes': df_classes,
-        'entropy': df_entropy
-    }
-
-    columns = ['examples', 'attributes', 'discrete_ratio', 'mean_entropy', 'mean_correlation', 'mean_skew', 'mean_kurtosis', 'outliers', 'classes', 'entropy']
-
-    meta_df = pd.DataFrame(table, columns=columns, index=[0])
-
-    print(meta_df)
-
-    meta_df = meta_df.fillna(0)
-    meta_df = meta_df.replace(np.inf, 100)
-    meta_df = meta_df.replace(-np.inf, -100)
-
-    knowledge_df = pd.read_csv('./results/meta-knowledge.csv')
-    del knowledge_df['Unnamed: 0']
-
-    knowledge_df = knowledge_df.fillna(0)
-    knowledge_df = knowledge_df.replace(np.inf, 100)
-    knowledge_df = knowledge_df.replace(-np.inf, -100)
-
-    db = knowledge_df['db']
-
-    # really strong influence over the knn algorithm, maybe it will not be used
-    # del knowledge_df['examples']
-    # del knowledge_df['attributes']
-    # del meta_df['examples']
-    # del meta_df['attributes']
-
-    y_cart = knowledge_df['cart_accuracy']
-    y_naive = knowledge_df['naive_accuracy']
-    y_neural = knowledge_df['neural_accuracy']
-
-    del knowledge_df['db']
-    del knowledge_df['cart_accuracy']
-    del knowledge_df['naive_accuracy']
-    del knowledge_df['neural_accuracy']
-    x = knowledge_df
-
-    # cart regression
-
-    linear = LinearRegression()
-    linear.fit(x, y_cart)
-    cart_pred = linear.predict(meta_df)
-
-    linear = LinearRegression()
-    linear.fit(x, y_naive)
-    naive_pred = linear.predict(meta_df)
-
-    linear = LinearRegression()
-    linear.fit(x, y_neural)
-    neural_pred = linear.predict(meta_df)
-
-    recomendation = ''
-
-    if cart_pred > naive_pred and cart_pred > neural_pred:
-        recomendation = 'cart'
-    if naive_pred > cart_pred and naive_pred > neural_pred:
-        recomendation = 'naive'
-    if neural_pred > cart_pred and neural_pred > naive_pred:
         recomendation = 'neural'
 
     return recomendation
@@ -327,7 +228,7 @@ print('---------------------------------------------------')
 
 poker = pd.read_csv('./test/poker.csv')
 
-print('database poker recomendation: ', linear(poker, 'class'))
+print('database poker recomendation: ', knn(poker, 'class'))
 print('cart_accuracy', cart('./test/poker.csv', 'class'))
 print('naive_accuracy', naive('./test/poker.csv', 'class'))
 print('neural_accuracy', neural('./test/poker.csv', 'class'))
@@ -337,7 +238,7 @@ print('---------------------------------------------------')
 
 agaricus = pd.read_csv('./test/agaricus-lepiota.csv')
 
-print('database agaricus recomendation: ', linear(agaricus, 'class'))
+print('database agaricus recomendation: ', knn(agaricus, 'class'))
 print('cart_accuracy', cart('./test/agaricus-lepiota.csv', 'class'))
 print('naive_accuracy', naive('./test/agaricus-lepiota.csv', 'class'))
 print('neural_accuracy', neural('./test/agaricus-lepiota.csv', 'class'))
@@ -347,7 +248,7 @@ print('---------------------------------------------------')
 
 car = pd.read_csv('./test/car.csv')
 
-print('database car recomendation: ', linear(car, 'class'))
+print('database car recomendation: ', knn(car, 'class'))
 print('cart_accuracy', cart('./test/car.csv', 'class'))
 print('naive_accuracy', naive('./test/car.csv', 'class'))
 print('neural_accuracy', neural('./test/car.csv', 'class'))
@@ -357,18 +258,38 @@ print('---------------------------------------------------')
 
 lung = pd.read_csv('./test/lung-cancer.csv')
 
-print('database lung recomendation: ', linear(lung, 'A1'))
+print('database lung recomendation: ', knn(lung, 'A1'))
 print('cart_accuracy', cart('./test/lung-cancer.csv', 'A1'))
 print('naive_accuracy', naive('./test/lung-cancer.csv', 'A1'))
 print('neural_accuracy', neural('./test/lung-cancer.csv', 'A1'))
 print('---------------------------------------------------')
 
-# database vehicle - nneds transformation
+# database vehicle - needs transformation
 
 vehicle = pd.read_csv('./test/vehicle.csv')
 
-print('database vehicle recomendation: ', linear(vehicle, 'A19'))
+print('database vehicle recomendation: ', knn(vehicle, 'A19'))
 print('cart_accuracy', cart('./test/vehicle.csv', 'A19'))
 print('naive_accuracy', naive('./test/vehicle.csv', 'A19'))
 print('neural_accuracy', neural('./test/vehicle.csv', 'A19'))
+print('---------------------------------------------------')
+
+# database messidor - perfect
+
+messidor = pd.read_csv('./test/messidor_features.csv')
+
+print('database messidor recomendation: ', knn(messidor, 'class'))
+print('cart_accuracy', cart('./test/messidor_features.csv', 'class'))
+print('naive_accuracy', naive('./test/messidor_features.csv', 'class'))
+print('neural_accuracy', neural('./test/messidor_features.csv', 'class'))
+print('---------------------------------------------------')
+
+# database iris - needs transformation
+
+iris = pd.read_csv('./test/iris.csv')
+
+print('database iris recomendation: ', knn(iris, 'class'))
+print('cart_accuracy', cart('./test/iris.csv', 'class'))
+print('naive_accuracy', naive('./test/iris.csv', 'class'))
+print('neural_accuracy', neural('./test/iris.csv', 'class'))
 print('---------------------------------------------------')
